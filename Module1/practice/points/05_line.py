93348d6ecfbb91c542faf45ff5eca8a3ac093bed
SergeySbornikov <70229550+SergeySbornikov@users.noreply.github.com>
2022-07-25 13:55:56 +0300

    Update 05_line.py

--- Module1/practice/points/05_line.py
+++ Module1/practice/points/05_line.py
@@ -3,10 +3,16 @@ class Point:
         self.x = x
         self.y = y
 
+    def dist_to(self, other_point):
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+
 
 # Ломаная линия задана произвольным количеством последовательных точек
-points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
+points = [Point(2, 4), Point(2, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
 # TODO: Найдите длину ломаной линии
+dist = 0
+for i in range(1, len(points)):
+    dist += points[i-1].dist_to(points[i])
 
-print("Длина ломаной линии = ", ...)
+print("Длина ломаной линии = ", dist)
